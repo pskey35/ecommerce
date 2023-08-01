bc60e3ed6baff7dc1bc95b396a64f988ec0b2c8b
pskey35 <anonimo35371@gmail.com>
2023-08-01 12:04:26 -0500

    este es un eccomerce - restaurant

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,17 @@ function User() {
       className="user"
     >
       <div className="user-container">
-        <Link to="/" className="user-salir">salir</Link>
+        <Link to="/" className="user-salir">
+        <svg class="ekis" viewBox="0 0 415.188 415.188" width="50" height="50"><path d="M412.861,78.976c3.404-6.636,2.831-14.159-0.15-20.404c0.84-7.106-1.02-14.321-7.746-19.855
+c-6.262-5.151-12.523-10.305-18.781-15.457c-11.005-9.055-28.237-11.913-38.941,0c-48.619,54.103-99.461,105.856-152.167,155.725
+c-39.185-36.605-78.846-72.713-118.223-108.868c-13.82-12.693-33.824-8.71-42.519,6.411c-12.665,6.286-22.931,14.481-31.42,28.468
+c-4.042,6.664-3.727,15.076,0,21.764c25.421,45.578,74.557,85.651,114.957,122.529c-5.406,4.839-10.772,9.724-16.287,14.461
+c-54.43,46.742-91.144,76.399-23.029,124.325c0.919,0.647,1.856,0.504,2.789,0.882c1.305,0.602,2.557,1.026,4.004,1.264
+c0.45,0.017,0.87,0.093,1.313,0.058c1.402,0.114,2.774,0.471,4.195,0.192c36.621-7.18,70.677-35.878,101.576-67.48
+c30.1,29.669,62.151,58.013,97.395,74.831c8.391,4.005,18.395,1.671,24.855-3.931c10.832,0.818,20.708-5.913,25.665-15.586
+c0.734-0.454,1.207-0.713,2.002-1.21c15.748-9.838,17.187-29.431,5.534-42.936c-26.313-30.492-54.284-59.478-82.798-87.95
+C316.426,196.043,380.533,141.939,412.861,78.976z"></path><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
+        </Link>
         <div className="user-img">
           <img src={image} alt="" />
         </div>
@@ -102,8 +112,39 @@ function Carrito(
       >
         <div className="carrito-primero">
           <span className="lineaCarrito"></span>
-          <Link style={{textDecoration:"none", background:"white"}} to="/" className="link-carrito" onClick={() =>setBolean(!bolean)}>
-            retro
+          <Link
+            style={{ textDecoration: "none" }}
+            to="/"
+            className="link-carrito"
+            onClick={() => setBolean(!bolean)}
+          >
+            <svg class="ekis" viewBox="0 0 415.188 415.188" width="50" height="50">
+                    <path d="M412.861,78.976c3.404-6.636,2.831-14.159-0.15-20.404c0.84-7.106-1.02-14.321-7.746-19.855
+c-6.262-5.151-12.523-10.305-18.781-15.457c-11.005-9.055-28.237-11.913-38.941,0c-48.619,54.103-99.461,105.856-152.167,155.725
+c-39.185-36.605-78.846-72.713-118.223-108.868c-13.82-12.693-33.824-8.71-42.519,6.411c-12.665,6.286-22.931,14.481-31.42,28.468
+c-4.042,6.664-3.727,15.076,0,21.764c25.421,45.578,74.557,85.651,114.957,122.529c-5.406,4.839-10.772,9.724-16.287,14.461
+c-54.43,46.742-91.144,76.399-23.029,124.325c0.919,0.647,1.856,0.504,2.789,0.882c1.305,0.602,2.557,1.026,4.004,1.264
+c0.45,0.017,0.87,0.093,1.313,0.058c1.402,0.114,2.774,0.471,4.195,0.192c36.621-7.18,70.677-35.878,101.576-67.48
+c30.1,29.669,62.151,58.013,97.395,74.831c8.391,4.005,18.395,1.671,24.855-3.931c10.832,0.818,20.708-5.913,25.665-15.586
+c0.734-0.454,1.207-0.713,2.002-1.21c15.748-9.838,17.187-29.431,5.534-42.936c-26.313-30.492-54.284-59.478-82.798-87.95
+C316.426,196.043,380.533,141.939,412.861,78.976z"></path>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g>
+                    </g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                </svg>
           </Link>
         </div>
         {itemCont > 0
@@ -112,13 +153,12 @@ function Carrito(
               <div className="carrito-caja">
                 {car.map((valor, indice) => {
                   return (
-                    //aqui falta arreglar la animacion no se como hacerlo
+                
                     <motion.div
                       key={indice}
                       layout
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
-                  
                       className={`item-container item-${indice}`}
                     >
                       <div className="item">
@@ -219,24 +259,23 @@ export default function App() {
     a.push(data);
     setCar(a);
 
-
     //el de abajo es el precio total de todos los productos
     a.map((e) => setCarroCont(carroCont + e.precio));
     setItemCont(itemCont + 1); //el de aqui cuenta cuantos items hay en el carrito
 
     setCont(cont + 1); //contador de notificacion
 
-    /*aqui cada vez que se da click se pone color verde 
+    /*aqui cada vez que se da click se pone color verde
      y luego de 180ms se pone normal.
     esto lo usaba con hover pero en los telefonos al no haber hover
     se pintaba cada vez que se hacia click y si querias agregar al carrito
     mas de un menu del mismo tipo no se veia diferencia
     */
-    const boton = document.querySelector(".right-boton")
+    const boton = document.querySelector(".right-boton");
     boton.style.background = "rgb(189, 124, 2)";
-    setTimeout(()=>{
-      boton.style.background = "initial"
-    },180)
+    setTimeout(() => {
+      boton.style.background = "initial";
+    }, 180);
   };
   const location = useLocation();
   return (
@@ -324,49 +363,49 @@ function Card({ data }) {
     desc: "lorem ipsum dolor aismet",
     puntuacion: 3.4,
     src: "./assets/img/food1.png",
-    id:1
+    id: 1,
   }, {
     nombre: "cordero",
     precio: 58,
     desc: "lorem ipsum dolor aismet",
     puntuacion: 3.4,
     src: "./assets/img/food2.png",
-    id:2
+    id: 2,
   }, {
     nombre: "res",
     precio: 11,
     desc: "lorem ipsum dolor aismet",
     puntuacion: 3.4,
     src: "./assets/img/food3.png",
-    id:3
+    id: 3,
   }, {
     nombre: "pescado",
     precio: 16,
     desc: "lorem ipsum dolor aismet",
     puntuacion: 3.4,
     src: "./assets/img/food4.png",
-    id:4
+    id: 4,
   }, {
     nombre: "cabrito",
     precio: 14,
     desc: "lorem ipsum dolor aismet",
     puntuacion: 3.4,
     src: "./assets/img/food5.png",
-    id:5
+    id: 5,
   }, {
     nombre: "papas",
     precio: 24,
     desc: "lorem ipsum dolor aismet",
     puntuacion: 3.4,
     src: "./assets/img/food6.png",
-    id:6
+    id: 6,
   }, {
     nombre: "alitas",
     precio: 22,
     desc: "lorem ipsum dolor aismet",
     puntuacion: 3.4,
     src: "./assets/img/food7.png",
-    id:7
+    id: 7,
   }];
 
   return (
